3ce83603da21b24b612a749d2d868397d00ace49
Josh Kurz <josh.kurz@turner.com>
2013-12-02 01:33:46 -0500

    feat(dynamicTemplate.js) added an example for dynamic templates
    Signed-off-by: Josh Kurz <josh.kurz@turner.com>

---
+++ directives/examples/dynamicTemplate.js
@@ -0,0 +1,40 @@
+// <div ng-app="demoApp" ng-controller="demoCtrl">
+//     <script id="/test1.tpl.html" type="text/ng-template">
+//       <div>{{test}}</div>
+//     </script>
+
+//     <script id="/test2.tpl.html" type="text/ng-template">
+//       <div>{{test}} plus another {{test}}</div>
+//     </script>
+    
+//     <button ng-click="templateVar = '/test1.tpl.html'">Test 1</button>
+//     <button ng-click="templateVar = '/test2.tpl.html'">Test 2</button>
+//     {{templateVar}}
+//     <div dynamic-template template-var="{{templateVar}}"></div>
+// </div>
+
+angular.module('demoApp', [])
+.controller('demoCtrl', function($scope){
+    $scope.templateVar = '/test1.tpl.html';
+})
+.directive('dynamicTemplate', ['$compile', '$templateCache',         function($compile, $templateCache){
+    return {  
+        scope: { templateVar: '@'},
+        link: function(scope, element, attrs){
+            
+            scope.test = 'Hello World';
+            scope.$watch('templateVar', function(newV,oldV){
+
+                if(newV !== undefined){
+                    var newElement = $compile($templateCache.get(newV))(scope);
+                    element.html(newElement);
+
+                }
+
+            });
+
+        }
+
+    }
+
+}]);
