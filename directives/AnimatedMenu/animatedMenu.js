92f88cec9f9fce3119eb53cb482a9ed0679eb687
Josh Kurz <josh.kurz@turner.com>
2013-12-01 19:17:57 -0500

    docs(animatedMenu.js) added some different versions of link and compile functions available that peforem the same tasks
    Signed-off-by: Josh Kurz <josh.kurz@turner.com>

--- directives/AnimatedMenu/animatedMenu.js
+++ directives/AnimatedMenu/animatedMenu.js
@@ -1,21 +1,61 @@
 angular.module('Menus', [])
 .controller('menuCtrl', function($scope){
     $scope.hello = 'Hello';
+    $scope.hello2 = ' World';
 })
-.directive('animatedMenu', ['$animate', function($animate){
+.directive('animatedMenu', [function(){
     return{
         restrict: 'EA',
         replace: true,
+        transclude: true,
         template: function(tElem, tAttrs){
             return '<div class="animated-menu animated-menu-vertical animated-menu-left">' +
                      '{{hello}}'  +
                     '</div>';
         },
-        link: function(scope, elem, attrs){
-
+        link: function(scope, elem, attrs, controller, transcludeFn){
+          
+          var clonedElement = transcludeFn(function(clone){
+            return clone;
+          });
+          elem.append(clonedElement);
           scope.showMenu = function() {
               elem.toggleClass('animated-menu-push-toright' );
           };
       }
     };
 }]);
+
+// link: {
+//   pre: function(scope, elem, attrs, controller, transcludeFn){
+//     scope.showMenu = function() {
+//       elem.toggleClass('animated-menu-push-toright' );
+//     };
+//   },
+//   post: function(scope,elem,attrs,controller,transcludeFn){
+//     var clonedElement = transcludeFn(function(clone){
+//       return clone;
+//     });
+//     elem.append(clonedElement);
+//     scope.showMenu();
+//   }
+// }
+
+// compile: function(tElem, tAttrs){
+           
+//     return {
+//         pre: function(scope, elem, attrs, controller, transcludeFn){
+//           scope.showMenu = function() {
+//             elem.toggleClass('animated-menu-push-toright' );
+//           };
+//         },
+//         post: function(scope,elem,attrs,controller,transcludeFn){
+//          var clonedElement = transcludeFn(function(clone){
+//             return clone;
+//           });
+//           elem.append(clonedElement);
+
+//           scope.showMenu();
+//         }
+//     }
+// }
