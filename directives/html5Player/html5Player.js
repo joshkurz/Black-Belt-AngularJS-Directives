885c1aef4cfcbf0a4aeb444e2111f7cfb667302a
Josh Kurz <josh.kurz@turner.com>
2013-12-20 22:08:34 -0500

    fix(html5Player) put the 5 in html5player
    Signed-off-by: Josh Kurz <josh.kurz@turner.com>

--- directives/html5Player/html5Player.js
+++ directives/html5Player/html5Player.js
@@ -1,16 +1,29 @@
-angular.module('AngularBlackBelt.html5Player', [])
-.directive('html5Player', [function(){
-	return {
-		restrict: 'EA',
-		replace: true,
-		scope: {
-			videoConfig: '='
-		}
-		compile: function(tElem, tAttrs){
+angular.module('AngularBlackBelt.htmlPlayer', ['directives/htmlPlayer/htmlPlayer.tpl.html', 'directives/htmlPlayer/youtubeHtmlPlayer.tpl.html'])
+.directive('htmlPlayer', ['$sce',function($sce) {
+    return {
+        restrict: 'A',
+        templateUrl: function(tElem, tAttrs){
 
-			return(scope, element, attrs){
-				
-			}
-		}
-	}
-}])
+            if (!tAttrs.templateUrl){
+                 throw new Error('Must Give the htmlPlayer a templateUrl to look for.');
+            }
+            
+            return tAttrs.templateUrl;
+        },
+        replace: 'element',
+        scope: {
+            videoConfig: '='
+        },
+        link: {
+                pre: function(scope,element,attrs){
+                    scope.trustSrc = function(ext) {
+                      return $sce.trustAsResourceUrl(scope.videoConfig.filePath + ext);
+                    };
+                },
+                post: function(scope, element, attrs) {
+                 
+                  setTimeout(function(){element.mediaelementplayer();},1000);
+                }
+        }
+    };
+}]);
