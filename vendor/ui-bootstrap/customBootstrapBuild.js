0c49d5f392c8555529f9864c78ee929974444135
Josh Kurz <josh.kurz@turner.com>
2013-12-24 17:18:07 -0500

    feat(demo) added video players to a dropdown menu
    Signed-off-by: Josh Kurz <josh.kurz@turner.com>

--- vendor/ui-bootstrap/customBootstrapBuild.js
+++ vendor/ui-bootstrap/customBootstrapBuild.js
@@ -1,4 +1,4 @@
-angular.module('ui.bootstrap', ['ui.bootstrap.position', 'ui.bootstrap.bindHtml', 'template/typeahead/typeahead-popup.html', 'template/typeahead/typeahead-match.html'])
+angular.module('ui.bootstrap', ['ui.bootstrap.position', 'ui.bootstrap.bindHtml', 'template/typeahead/typeahead-popup.html', 'template/typeahead/typeahead-match.html', 'ui.bootstrap.dropdownToggle'])
 
 /**
  * A helper service that can parse typeahead's syntax (string provided by users)
@@ -452,5 +452,58 @@ angular.module("template/typeahead/typeahead-popup.html", []).run(["$templateCac
     "        <div typeahead-match index=\"$index\" match=\"match\" query=\"query\" template-url=\"templateUrl\"></div>\n" +
     "    </li>\n" +
     "</ul>");
+}]);
+
+/*
+ * dropdownToggle - Provides dropdown menu functionality in place of bootstrap js
+ * @restrict class or attribute
+ * @example:
+   <li class="dropdown">
+     <a class="dropdown-toggle">My Dropdown Menu</a>
+     <ul class="dropdown-menu">
+       <li ng-repeat="choice in dropChoices">
+         <a ng-href="{{choice.href}}">{{choice.text}}</a>
+       </li>
+     </ul>
+   </li>
+ */
+
+angular.module('ui.bootstrap.dropdownToggle', []).directive('dropdownToggle', ['$document', '$location', function ($document, $location) {
+  var openElement = null,
+      closeMenu   = angular.noop;
+  return {
+    restrict: 'CA',
+    link: function(scope, element, attrs) {
+      scope.$watch('$location.path', function() { closeMenu(); });
+      element.parent().bind('click', function() { closeMenu(); });
+      element.bind('click', function (event) {
+
+        var elementWasOpen = (element === openElement);
+
+        event.preventDefault();
+        event.stopPropagation();
+
+        if (!!openElement) {
+          closeMenu();
+        }
+
+        if (!elementWasOpen && !element.hasClass('disabled') && !element.prop('disabled')) {
+          element.parent().addClass('open');
+          openElement = element;
+          closeMenu = function (event) {
+            if (event) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            $document.unbind('click', closeMenu);
+            element.parent().removeClass('open');
+            closeMenu = angular.noop;
+            openElement = null;
+          };
+          $document.bind('click', closeMenu);
+        }
+      });
+    }
+  };
 }]);
 
