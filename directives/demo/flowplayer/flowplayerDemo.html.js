7b41a710f230fd7ae90a5d1a6d05791db7cfa988
Josh Kurz <jkurz25@gmail.com>
2014-04-26 22:50:28 -0400

    update(demo): made the demo cooler
    Signed-off-by: Josh Kurz <jkurz25@gmail.com>

--- directives/demo/flowplayer/flowplayerDemo.html.js
+++ directives/demo/flowplayer/flowplayerDemo.html.js
@@ -5,7 +5,7 @@ angular.module("directives/demo/flowplayer/flowplayerDemo.tpl.html", []).run(["$
     "    <span>\n" +
     "        <h1>Flowplayer</h1>\n" +
     "    </span>\n" +
-    "    <p>A demo showcasing the power of <a href=\"https://flowplayer.org/\">flowplayer.js</a> and <a href=\"https://angularjs.org/\">AngularJS</a></p>\n" +
+    "    <p>A demo showcasing the power of <a href=\"https://flowplayer.org/\">flowplayer.js</a> and <a href=\"https://angularjs.org/\">Angular<span class=\"jsRed\">JS</span></a></p>\n" +
     "    <p>This directive can work with more libraries than just flowplayer. Its purpose is to showcase how important the $compile service can be to a directive.</p>\n" +
     "    <p>All of the different types of flowplayers that are available here are being created by the same directive.<p>\n" +
     "  </div>\n" +
